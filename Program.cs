50b4bacf087f83f58b0070354445d6a89341a623
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-06-19 10:57:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word.");
+
+            String input = Console.ReadLine();
+
+            String upperInput = input.ToUpper();
+
+            foreach (char c in upperInput)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
